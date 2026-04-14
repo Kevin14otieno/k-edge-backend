9bde13ffb346a590d2827e1264e0645421c4ae5c
Kevin14otieno <92913295+Kevin14otieno@users.noreply.github.com>
2026-04-14 11:32:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def download():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port),  replace here
+    app.run(host="0.0.0.0", port=port),
